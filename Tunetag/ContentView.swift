d8879c2bf73b5e4682e513f0804732224350c13b
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-07 20:45:21 +0800

    Initial Commit

---
+++ Tunetag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tunetag
+//
+//  Created by シン・ジャスティン on 2023/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tunetag/TunetagApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunetagApp.swift
+//  Tunetag
+//
+//  Created by シン・ジャスティン on 2023/09/07.
+//
+
+import SwiftUI
+
+@main
+struct TunetagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
